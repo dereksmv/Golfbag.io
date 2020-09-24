fa45c3e42b8d286697b6c2cb498a99ed8345e86a
lotg515 <dereksmv@gmail.com>
2020-09-24 18:40:55 -0500

    Tailwind Initialized

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+  <div class="flex-shrink-0">
+    <img class="h-12 w-12" alt="ChitChat Logo"></img>
+  </div>
+  <div class="ml-6 pt-1">
+    <h4 class="text-xl text-gray-900 leading-tight">ChitChat</h4>
+    <p class="text-base text-gray-600 leading-normal">You have a new message!</p>
+  </div>
+</div>
   );
 }
-
 export default App;
