d7aae2a5e662f46d83391217e5286c568b964fbc
lotg515 <dereksmv@gmail.com>
2020-09-28 23:28:58 -0500

    deployment configured

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const port = process.env.PORT || 5000; // process.env.port is Heroku's port if y
 app.listen(port, () => console.log(`Server up and running on port ${port}`));
 
 app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
