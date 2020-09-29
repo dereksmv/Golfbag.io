48d177366801e25b4decd4369fc039616e82156e
lotg515 <dereksmv@gmail.com>
2020-09-28 23:38:16 -0500

    deployment configured

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ app.listen(port, () => console.log(`Server up and running on port ${port}`));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/build/index.html'));
 });
 
 app.use("/api/golf_clubs", golfclubs);
