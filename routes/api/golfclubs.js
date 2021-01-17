d026e1b9b96e3af03696bca4dc5df781f5577798
lotg515 <dereksmv@gmail.com>
2021-01-17 00:03:48 -0600

    removed cloudinary refs

--- routes/api/golfclubs.js
+++ routes/api/golfclubs.js
@@ -5,25 +5,9 @@ const golfClubs = require("../../models/Golfclubs")
 const golfBag = require("../../services/services")
 const db = require("../../services/databaseCalls")
 const multer = require("multer");
-const cloudinary = require("cloudinary");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET
-    });
-    const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    folder: "demo",
-    allowedFormats: ["jpg", "png"],
-    transformation: [{ width: 500, height: 500, crop: "limit" }]
-    });
-    const upload = multer({ storage: storage }); 
-
-
-router.post("/create_new", upload.single('image'), (req, res) => {
+router.post("/create_new", (req, res) => {
     const image = {};
     console.log("ping")
     console.log(req.body)
