7e50c1bbfda3add7e869a47027b6f71b664a6d14
Daniel Veza <veez@hotmail.co.nz>
2021-03-13 10:26:14 +1300

    Rebuilt MainNav as a functional component with Material & Styled compoenents

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import CreateABag from "./pages/CreateABag/CreateABag"
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import MainNav from './components/navigation/mainNav';
 import CreateAGolfer from './pages/CreateABag/CreateAGolfer';
 import PlayersView from "./pages/CreateABag/PlayersView"
+import {Container} from "@material-ui/core";
 
 function App() {
   return (
     <div className="bg-gray-200">
+      <main>
+        <Container>
+          <Router>
+            <Route path="/create/Bag" component={CreateABag}/>
+            <Route path="/create/Golfer" component={CreateAGolfer}/>
+            <Route path="/players" component={PlayersView}/>
+          </Router>
+          <p>Homepage...</p>
+        </Container>
+      </main>
       <MainNav/>
-        <Router>
-          <Route path="/create/Bag" component={CreateABag}/>
-          <Route path="/create/Golfer" component={CreateAGolfer}/>
-          <Route path="/players" component={PlayersView}/>
-        </Router>
     </div>
   );
 }
